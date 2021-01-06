ab380e10b7cdccddcfc082e4b5001940882e01af
Miguel Alvarez <kamapu78@gmail.com>
2021-01-06 14:00:03 +0000

    version 0.1.1

--- R/write_rmd.R
+++ R/write_rmd.R
@@ -1,44 +1,59 @@
 #' @name write_rmd
 #' 
-#' @title Writing yaml headers
+#' @title Writing R-Markdown Documents
 #' 
 #' @description 
-#' This function generates yaml headers and Rmarkdown files by including the
-#' settings as arguments of the function.
+#' This function generates YAML headers and R-Markdown documents by including
+#' the settings as arguments of the function.
 #' Comments and pieces of header can be also added through the argument
 #' `append`.
 #' 
-#' @param ... Named arguments to be inserted in the yaml head. These arguments
+#' @param ... Named arguments to be inserted in the YAML header. These arguments
 #'     may be inserted either as vectors or lists.
-#' @param append A piece of code to be appended in the head. For instance, it
+#' @param append A piece of code to be appended in the header. For instance, it
 #'     can contain commented code.
-#' @param body The content of the document that will be inserted after the head.
+#' @param body The content of the document that will be inserted after the
+#'     header.
 #' @param filename A character value with the name of the file to be written
-#'     (usually a *.Rmd file). If missing, no file will be written by the
+#'     (usually a *.Rmd file). If missing, no file will be written by this
 #'     function.
 #' 
 #' @return 
 #' A character vector of class `rmd_doc` and, if argument set for parameter
 #' `filename`, an Rmd file.
 #' 
+#' @examples 
+#' \dontrun{
+#' my_document <- write_rmd(
+#' 		title = "Sample Document", author = "Miguel Alavarez",
+#' 		output = "html_document",
+#' 		body = txt_body(
+#' 				"# Intro",
+#' 				"",
+#' 				"This is just an example."))
+#' my_document
+#' }
+#' 
 #' @export write_rmd
 #' 
-write_rmd <- function(..., append, body="", filename) {
-	Head <- write_yaml(list(...))
+write_rmd <- function(..., append, body = "", filename) {
+	OUT <- list()
+	OUT$header <- list(...)
 	# Append
 	if(!missing(append))
-		Head <- c(Head, paste0(append, "\n"))
-	OUT <- c("---\n", Head, "---\n\n", body, "\n")
+		OUT$append <- txt_body(append) else OUT$append <- NULL
+	OUT$body <- body
 	# Write output file
 	if(!missing(filename)) {
 		con <- file(filename, "wb")
-		writeBin(charToRaw(paste(OUT, collapse="")),
+		writeBin(charToRaw(paste0(c("---\n", write_yaml(OUT$header), OUT$append,
+										"\n---\n\n", OUT$body, "\n"),
+								collapse="")),
 				con)
 		close(con)
 	}
 	# Return strings
-	names(OUT) <- NULL
-	class(OUT) <- c("rmd_doc", "character")
+	class(OUT) <- c("rmd_doc", "list")
 	invisible(OUT)
 }
 
--- inst/doc/yamlme-intro.R
+++ inst/doc/yamlme-intro.R
@@ -6,30 +6,35 @@
 library(yamlme)
 
 ## -----------------------------------------------------------------------------
-cat(write_rmd(title = "My first document"))
+my_document <- write_rmd(title = "My first document")
+my_document
 
 ## -----------------------------------------------------------------------------
-cat(write_rmd(description = c("|",
-						"This text starts with a vertical line",
-						"and will be thus used as a description",
-						"in the head.")))
+my_document <- write_rmd(description = c("|",
+				"This text starts with a vertical line",
+				"and will be thus used as a description",
+				"in the head."))
+my_document
 
 ## -----------------------------------------------------------------------------
 the_title <- "Introduction to this package"
-cat(write_rmd(
-				title = the_title,
-				vignette = c(">",
-						paste0("%\\VignetteIndexEntry{", the_title, "}"),
-						"%\\VignetteEngine{knitr::rmarkdown}",
-						"%\\VignetteEncoding{UTF-8}")))
+my_document <- write_rmd(
+		title = the_title,
+		vignette = c(">",
+				paste0("%\\VignetteIndexEntry{", the_title, "}"),
+				"%\\VignetteEngine{knitr::rmarkdown}",
+				"%\\VignetteEncoding{UTF-8}"))
+my_document
 
 ## -----------------------------------------------------------------------------
-cat(write_rmd(author=c("- name: Miguel Alvarez",
-						"url: https://kamapu.github.io/",
-						"- name: Bisrat H. Gebrekhidan")))
+my_document <- write_rmd(author=c("- name: Miguel Alvarez",
+				"url: https://kamapu.github.io/",
+				"- name: Bisrat H. Gebrekhidan"))
+my_document
 
 ## -----------------------------------------------------------------------------
-cat(write_rmd(output=list(pdf_document="default")))
+my_document <- write_rmd(output=list(pdf_document="default"))
+my_document
 
 ## -----------------------------------------------------------------------------
 my_document <- write_rmd(title = "Mi First Document", author = "My Name",
@@ -43,42 +48,26 @@ my_document <- write_rmd(title = "Mi First Document", author = "My Name",
 				"- Start the coffe mashine",
 				"- Start the computer",
 				"- Read mails"))
-cat(my_document)
+my_document
 
 ## ----eval = FALSE-------------------------------------------------------------
 #  render_rmd(input = my_document, output_file = "my_document")
 
 ## -----------------------------------------------------------------------------
-my_template <- function(
-		title = "My Awesome Document",
+my_template <- write_rmd(
+		title = "Example HTML document",
 		author = "My Self",
 		output = "html_document",
 		body = txt_body(
 				"# Introduction",
 				"",
-				"This is just an example."),
-		...) {
-	write_rmd(
-			title = title,
-			author = author,
-			output = output,
-			body = body,
-			...)
-}
+				"This is just an example."))
+my_template
 
 ## -----------------------------------------------------------------------------
-cat(my_template())
-
-## -----------------------------------------------------------------------------
-cat(my_template(
-				output = "pdf_document",
-				append = "# this is a modified version"
-))
-
-## ----eval = FALSE-------------------------------------------------------------
-#  # Modify only the title of the template and render
-#  render_rmd(my_template(title = "Example HTML document"))
-#  
-#  # Modify title and output format before rendering
-#  render_rmd(my_template(title = "Example PDF document", output = "pdf_document"))
+my_template <- update(my_template, 
+		title = "Example PDF document",
+		output = "pdf_document",
+		append = "# this is a modified version")
+my_template
 
